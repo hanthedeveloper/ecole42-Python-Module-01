044bbc74de33cea50e92e0eb9419d749a2362d9c
han <bayramincel55@gmail.com>
2026-09-22 12:19:46 +0300

    Delete m00_ex00-ex07 directory

--- m00_ex00-ex07/ft_plant_age.py
+++
@@ -1,6 +0,0 @@
-def ft_plant_age() -> None:
-    day = int(input("Enter plant age in days: "))
-    if (day > 60):
-        print("Plant is ready to harvest!")
-    else:
-        print("Plant needs more time to grow.")
